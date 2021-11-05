52cbcc5a1f959b79d08b214a2741599d6945ef00
Michael Joseph <michaeljoseph195@gmail.com>
2021-11-05 00:04:17 +0000

    preliminary refactor of db.py and classes.py -> scp.py

--- db.py
+++ db.py
@@ -1,191 +1,242 @@
-# handles the SCP database
-# assume that we need to handle objects of the SCP class for now
-# scp.number -> id_number and scp.exists -> exists_online in this file because number & exists are keywords in sqlite3
+"""
+Handles the SCP database
+
+scp.number -> id_number and scp.exists -> exists_online in this file because number
+& exists are keywords in sqlite3
+"""
+
 import sqlite3
 from datetime import datetime
 from os import path
 
-from classes import SCP
-
-db_path = path.dirname(path.realpath(__file__)) + "/scps.db"
+from scp import SCP
 
 
-# create a function that creates the scp table in the database, only run this once
-def set_up_database():
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
+class SCPDatabase():
+    """
+    ORM that handles all interactions with the SCP database
+    """
 
-    # take note that booleans should be stored as integer values 1 or 0
-    # rating is stored as text since we need a way to know if it is N/A
-    # id_number can remain as integer since we can always know its value
-    c.execute("""
-        CREATE TABLE scps (
-        id_number text,
-        name text,
-        object_class text,
-        rating text,
-        URL text,
-        has_image integer,
-        have_read integer,
-        dont_want_to_read integer,
-        exists_online integer,
-        is_favorite integer,
-        unusual_format integer,
-        last_updated text,
-        read_later integer)
-        """)
+    def __init__(self, db_name):
+        self.db_path = path.dirname(path.realpath(__file__)) + '/' + db_name
+        self.set_up_database()
 
-    conn.commit()
-    conn.close()
+    def set_up_database(self):
+        """
+        Create a new database if it doesn't already exist.
+        """
+        if path.exists(self.db_path):
+            return
 
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
 
-# create a function that updates an scp in the database
-def update_scp(scp):
-    # takes an SCP class as input
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-
-    # get the current day and store it in the format "DD-MM-YYYY"
-    now = datetime.now()
-    last_updated = f"{now.day}-{now.month}-{now.year}"
-
-    c.execute("""UPDATE scps SET
-        id_number = :id_number,
-        name = :name,
-        object_class = :object_class,
-        rating = :rating,
-        URL = :URL,
-        has_image = :has_image,
-        have_read = :have_read,
-        dont_want_to_read = :dont_want_to_read,
-        exists_online = :exists_online,
-        is_favorite = :is_favorite,
-        unusual_format = :unusual_format,
-        last_updated = :last_updated,
-        read_later = :read_later
-        
-        WHERE id_number=:id_number""",
-        {
-        "id_number": scp.number,
-        "name": scp.name,
-        "object_class": scp.object_class,
-        "rating": scp.rating,
-        "URL": scp.URL,
-        "has_image": scp.has_image,
-        "have_read": scp.have_read,
-        "dont_want_to_read": scp.dont_want_to_read,
-        "exists_online": scp.exists,
-        "is_favorite": scp.is_favorite,
-        "unusual_format": scp.unusal_format,
-        "last_updated": last_updated,
-        "read_later": scp.read_later
-        }
-    )
-
-    conn.commit()
-    conn.close()
-    # 1 indicates that the scp was updated successfully
-    return 1
-
-
-# create a function which returns the data for a given scp number
-def get_scp(scp_id_number):
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-
-    try:
-        c.execute("SELECT * FROM scps WHERE id_number={}".format(scp_id_number))
-        # fetchall returns a list containing a dict of all field for scp if it exists, otherwise nothing
-        data = c.fetchall()[0]
-    except IndexError:
-        # -1 indicates that the scp does not exist in the database
-        return -1
-
-    # parse the data
-    id_number = data[0]
-    name = data[1]
-    object_class = data[2]
-    rating = data[3]
-    URL = data[4]
-    has_image = data[5]
-    have_read = data[6]
-    dont_want_to_read = data[7]
-    exists_online = data[8]
-    is_favorite = data[9]
-    unusual_format = data[10]
-    last_updated = data[11]
-    read_later = data[12]
-
-    output_scp = SCP(id_number, name, object_class, rating, URL, has_image, have_read, dont_want_to_read,
-                     exists_online, is_favorite, unusual_format, last_updated, read_later)
-
-    return output_scp
-
-
-# create function to add an scp to the database
-def add_scp(scp):
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
-
-    # get the current day and store it in the format "DD-MM-YYYY"
-    now = datetime.now()
-    last_updated = f"{now.day}-{now.month}-{now.year}"
-
-    # first check if the scp already exists in the database
-    check_db = get_scp(scp.number)
-    if check_db == -1:
-        c.execute("INSERT INTO scps VALUES (:id_number, :name, :object_class, :rating, :URL, :has_image, :have_read,\
-            :dont_want_to_read, :exists_online, :is_favorite, :unusual_format, :last_updated, :read_later)",
-            {
-            "id_number": scp.number,
-            "name": scp.name,
-            "object_class": scp.object_class,
-            "rating": scp.rating,
-            "URL": scp.URL,
-            "has_image": scp.has_image,
-            "have_read": scp.have_read,
-            "dont_want_to_read": scp.dont_want_to_read,
-            "exists_online": scp.exists,
-            "is_favorite": scp.is_favorite,
-            "unusual_format": scp.unusal_format,
-            "last_updated": last_updated,
-            "read_later": scp.read_later
-            }
+        # booleans are stored as ints
+        # rating is stored as text because it can be N/A
+        cursor.execute(
+            """
+            CREATE TABLE scps (
+                id_number text,
+                name text,
+                object_class text,
+                rating text,
+                URL text,
+                has_image integer,
+                have_read integer,
+                dont_want_to_read integer,
+                exists_online integer,
+                is_favorite integer,
+                unusual_format integer,
+                last_updated text,
+                read_later integer
+            )
+            """
         )
-    else:
+
         conn.commit()
         conn.close()
-        # -1 indicates that the scp was already in the database
-        return -1
 
-    conn.commit()
-    conn.close()
-    # 1 indicates that the scp was successfully inserted
-    return 1
+    def update_scp(self, scp):
+        """
+        Update an SCP in the database
+        """
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
 
-# create a function to search the database and return the numbers of all SCPs in it
-# optionally include a list of additional data points desired e.g.
-#   additional_data=["have_read", "dont_want_to_read"]
-# this function returns the data for each scp as a dict
-def get_available_scp_numbers(additional_data=[]):
-    conn = sqlite3.connect(db_path)
-    c = conn.cursor()
+        now = datetime.now()
+        last_updated = f"{now.day}-{now.month}-{now.year}"
 
-    additional_data_string = ", ".join(additional_data)
-    if additional_data_string:
-        additional_data_string = ", " + additional_data_string
+        cursor.execute(
+            """
+            UPDATE scps SET
+                id_number = :id_number,
+                name = :name,
+                object_class = :object_class,
+                rating = :rating,
+                URL = :URL,
+                has_image = :has_image,
+                have_read = :have_read,
+                dont_want_to_read = :dont_want_to_read,
+                exists_online = :exists_online,
+                is_favorite = :is_favorite,
+                unusual_format = :unusual_format,
+                last_updated = :last_updated,
+                read_later = :read_later
 
-    c.execute("SELECT id_number{} FROM scps".format(additional_data_string))
-    # returns a list of tuples
-    db_data = c.fetchall()
+                WHERE id_number=:id_number
+            """,
+            {
+                "id_number": scp.number,
+                "name": scp.name,
+                "object_class": scp.object_class,
+                "rating": scp.rating,
+                "URL": scp.URL,
+                "has_image": scp.has_image,
+                "have_read": scp.have_read,
+                "dont_want_to_read": scp.dont_want_to_read,
+                "exists_online": scp.exists,
+                "is_favorite": scp.is_favorite,
+                "unusual_format": scp.unusal_format,
+                "last_updated": last_updated,
+                "read_later": scp.read_later
+            }
+        )
 
-    # now we need to dynamically create a dictionary for each scps key-value pairs and collect those together in a list
-    # as output
-    output_data= []
-    for scp in db_data:
-        scp_dict = {"number": scp[0]}
-        for i, data_point in enumerate(additional_data):
-            scp_dict[data_point] = scp[i+1]
-        output_data.append(scp_dict)
+        conn.commit()
+        conn.close()
+        return 1
 
-    return output_data
+    def get_scp(self, scp_id_number):
+        """
+        Returns data for a given scp number
+        """
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("SELECT * FROM scps WHERE id_number={}".format(scp_id_number))
+            # fetchall returns a list containing a dict of all field for scp if it 
+            # exists, otherwise nothing
+            data = cursor.fetchall()[0]
+        except IndexError:
+            # -1 indicates that the scp does not exist in the database
+            return -1
+
+        # parse the data
+        id_number = data[0]
+        name = data[1]
+        object_class = data[2]
+        rating = data[3]
+        URL = data[4]
+        has_image = data[5]
+        have_read = data[6]
+        dont_want_to_read = data[7]
+        exists_online = data[8]
+        is_favorite = data[9]
+        unusual_format = data[10]
+        last_updated = data[11]
+        read_later = data[12]
+
+        output_scp = SCP(
+            id_number,
+            name,
+            object_class,
+            rating,
+            URL,
+            has_image,
+            have_read,
+            dont_want_to_read,
+            exists_online,
+            is_favorite,
+            unusual_format,
+            last_updated,
+            read_later
+        )
+
+        return output_scp
+
+    def add_scp(self, scp):
+        """
+        Add an SCP to the database
+        """
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        now = datetime.now()
+        last_updated = f"{now.day}-{now.month}-{now.year}"
+
+        if self.get_scp(scp.number) == -1:
+            cursor.execute(
+                """
+                INSERT INTO scps VALUES (
+                    :id_number,
+                    :name,
+                    :object_class,
+                    :rating,
+                    :URL,
+                    :has_image,
+                    :have_read,
+                    :dont_want_to_read,
+                    :exists_online,
+                    :is_favorite,
+                    :unusual_format,
+                    :last_updated,
+                    :read_later
+                )
+                """,
+                {
+                    "id_number": scp.number,
+                    "name": scp.name,
+                    "object_class": scp.object_class,
+                    "rating": scp.rating,
+                    "URL": scp.URL,
+                    "has_image": scp.has_image,
+                    "have_read": scp.have_read,
+                    "dont_want_to_read": scp.dont_want_to_read,
+                    "exists_online": scp.exists,
+                    "is_favorite": scp.is_favorite,
+                    "unusual_format": scp.unusal_format,
+                    "last_updated": last_updated,
+                    "read_later": scp.read_later
+                }
+            )
+        else:
+            conn.commit()
+            conn.close()
+            # -1 indicates that the scp was already in the database
+            return -1
+
+        conn.commit()
+        conn.close()
+        return 1
+
+    def get_available_scp_numbers(self, additional_data=[]):
+        """
+        Searches the database and returns the ids of all SCPs in it.
+
+        optionally include a list of additional data points to return, e.g:
+            additional_data=["have_read", "dont_want_to_read"]
+
+        This function returns the data for each scp as a dict.
+        """
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+
+        additional_data_string = ", ".join(additional_data)
+        if additional_data_string:
+            additional_data_string = ", " + additional_data_string
+
+        cursor.execute("SELECT id_number{} FROM scps".format(additional_data_string))
+        # returns a list of tuples
+        db_data = cursor.fetchall()
+
+        # now we need to dynamically create a dictionary for each scps key-value pairs 
+        # and collect those together in a list as output
+        output_data= []
+        for scp in db_data:
+            scp_dict = {"number": scp[0]}
+            for i, data_point in enumerate(additional_data):
+                scp_dict[data_point] = scp[i+1]
+            output_data.append(scp_dict)
+
+        return output_data
---
+++ scp.py
@@ -0,0 +1,61 @@
+class SCP:
+    def __init__(self,
+        number,
+        name,
+        object_class,
+        rating,
+        URL,
+        has_image,
+        have_read=0,
+        dont_want_to_read=0,
+        exists=1,
+        is_favorite=0,
+        unusual_format=0,
+        last_updated="Never",
+        read_later=False
+    ):
+        self.name = name
+        self.object_class = object_class
+        self.rating = rating
+        self.URL = URL
+        self.has_image = has_image
+        self.have_read = have_read
+        self.dont_want_to_read = dont_want_to_read
+        self.exists = exists
+        self.is_favorite = is_favorite
+        self.unusal_format = unusual_format
+        self.last_updated = last_updated
+        self.read_later = read_later
+
+        str_number = str(number)
+        while (len(str_number)) < 3:
+            str_number = '0' + str_number
+        self.number = str_number
+
+    def print(self, debug=False):
+        print(
+            f"""
+            number: {self.number}
+            name: {self.name}
+            object class: {self.object_class}
+            rating: {self.rating}
+            link: {self.URL}
+            """
+        )
+        if debug:
+            print(
+                f"""
+                have read: {self.have_read}
+                don't want to read: {self.dont_want_to_read}
+                exists: {self.exists}
+                is favorite: {self.is_favorite}
+                has unusual format: {self.unusal_format}
+                has image: {self.has_image}
+                last updated: {self.last_updated}
+                read later: {self.read_later}
+                """
+            )
+
+    def __repr__(self):
+        return "{} - {}\n{}\n".format(self.number, self.name, self.URL)
+
